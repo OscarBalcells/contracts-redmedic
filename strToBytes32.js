8fd1ba1079d2cebea855ce55a373b70b1d85ab23
Oscar Balcells Obeso <balcells.oscar@gmail.com>
2019-11-05 18:46:02 +0100

    Update strToBytes32.js

--- strToBytes32.js
+++ strToBytes32.js
@@ -1,3 +1,4 @@
+// @dev Converting an ASCII input string into bytes32 representation with padding
 const web3 = require("web3");
 const input = process.argv[2]; 
 const output = web3.utils.fromAscii(input).padEnd(66,"0")
