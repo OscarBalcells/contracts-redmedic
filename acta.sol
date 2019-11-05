88fc1e088c15a218a2e1c2e2d72aedffaf0627f1
Oscar Balcells Obeso <balcells.oscar@gmail.com>
2019-11-05 18:43:59 +0100

    Comments added to acta.sol

--- acta.sol
+++ acta.sol
@@ -2,12 +2,15 @@ pragma solidity ^0.5.1;
 
 contract Acta {
 
-  // Proposals:
-	// 1 => new member join
-	// 2 => kick member out
-	// 3 => change required amount of confirmations for a certain action id
-	// 4 => send x amount of contract's funds to specified address
+    /*
+     What does each proposal do? (by proposalId)
+	 1 => new member join
+	 2 => kick member out
+	 3 => change required amount of confirmations for executing an action
+	 4 => send x amount of contract's funds to specified address
+    */
 
+    //To notify external listeners of changes that have ocurred in the contract
 	event JoinRequest(bytes32 eventName, address who, bytes32 name, uint fee);
 	event ProposalSaved(bytes32 eventName, address proposer, uint proposalId, uint numData, bytes32 bytesData1, bytes32 bytesData2, address addrData);
 	event ProposalExecuted(bytes32 eventName, uint proposalId, uint numData, bytes32 bytesData1, bytes32 bytesData2, address addrData);
@@ -34,11 +37,11 @@ contract Acta {
 	mapping (address => Member) public members;
 	mapping (bytes32 => address) public nameToAddress;
 	mapping (uint => Proposal) public proposals;
-
 	uint public nRequiredVotes = 1;
 	uint public nProposals = 0;
 	uint public nMembers = 0;
 
+    // @dev the creator of the contract gets automatically put as a registered member
     constructor(bytes32 name, bytes32 gateway) public {
         members[msg.sender] = Member({
             name: name,
@@ -49,8 +52,13 @@ contract Acta {
         nMembers++;
     }
 
-    ///PROPOSAL CREATION
-
+    // @dev the Proposals get externally proposed from other functions so that it's easier to call them and save code
+    //     so they all get internally (hence private tag in function) organized and sent to this function which just adds them to the list
+    // @param proposalId The ID of the proposal. The ID specifies the exact goal of the proposal
+    // @param uint numData Used for: new nRequiredVotes (ID = 3) or balance to send (ID = 4)
+    // @param bytes32 bytesData1 Used for: name of member to be accepted (ID = 1) or name of member to be kicked out (ID = 2)
+    // @param bytes32 bytesData2 Used for: gateway (ID = 1)
+    // @param address addrData Used for: Setting address for all except for ID = 3
     function internalActionProposal(uint proposalId, uint numData, bytes32 bytesData1, bytes32 bytesData2, address addrData) private {
         proposals[nProposals] = Proposal({
             proposalId: proposalId,
@@ -63,32 +71,45 @@ contract Acta {
         nProposals++;
         emit ProposalSaved("ProposalSaved", msg.sender, proposalId, numData, bytesData1, bytesData2, addrData);
     }
-
+    
+    // @dev This function gets executed by any external Ethereum account, which wants to join the network. 
+    //      it has to pay a fee of 0.1 ether (to avoid spam requests) and after that it will get formalised as a proposal
+    // @param bytes32 name The Name it will have in the network
+    // @param bytes32 gateway The gateway it will have in the network
     function joinRequest(bytes32 name, bytes32 gateway) public payable {
 		require(isMember[msg.sender] == false);
 		require(msg.value >= 0.1 ether);
 		emit JoinRequest("JoinRequest", msg.sender, name, msg.value);
         internalActionProposal(1, 0, name, gateway, msg.sender);
     }
-
+    
+    // @dev The same as last function but it gets called by someone who is already a member
+    //      organization so that the new member doesn't have to pay any fee (invitation)
+    // @param address addr We add the address here, because the function doesn't get called
+    //        by the wannabe member but rather by the already a-member, so the new member's
+    //        address has to be specified
     function addMemberProposal(bytes32 name, bytes32 gateway, address addr) public {
-        require(isMember[msg.sender] == true);
-        require(isMember[addr] == false);
+        require(isMember[msg.sender] == true && isMember[addr] == false);
         internalActionProposal(1, 0, name, gateway, addr);
         votes[nProposals-1][msg.sender] = true;
         proposals[nProposals-1].nVotes++;
         if(nRequiredVotes == 1) executeProposal(nProposals-1);
     }
 
+    // @dev Gets called by a member who specifies the member he wants to kick
+    // @param bytes32 name The name of the member which the proposal aims to kick
+    // @param address addr The address of the member which the proposal aims to kick
     function kickMemberProposal(bytes32 name, address addr) public {
-        require(isMember[msg.sender] == true);
-        require(isMember[addr] == true);
+        require(isMember[msg.sender] == true && isMember[addr] = true);
         internalActionProposal(2, 0, name, "", addr);
         votes[nProposals-1][msg.sender] = true;
         proposals[nProposals-1].nVotes++;
         if(nRequiredVotes == 1) executeProposal(nProposals-1);
     }
 
+    // @dev Gets called by a member who wants to change the number of required votes
+    //      to have a proposal executed
+    // @param uint newRequirement The new required amount of necesary votes 
     function changeRequiredVotesProposal(uint newRequirement) public {
         require(isMember[msg.sender] == true);
         internalActionProposal(3, newRequirement, "", "", address(this));
@@ -97,6 +118,10 @@ contract Acta {
         if(nRequiredVotes == 1) executeProposal(nProposals-1);
     }
 
+    // @dev Gets called by a member. Proposal aims @amount of contract's funds to @receiverAddr
+    // @param uint amount The amount to be sent
+    // @param bytes32 receiverName Just so that members can know what the name of the funds's receiver is. For administrative purposes
+    // @param address receiverAddr The address of the receiver of the funds
     function transferFundsProposal(uint amount, bytes32 receiverName, address receiverAddr) public {
         require(isMember[msg.sender] == true);
         internalActionProposal(4, amount, receiverName, "", receiverAddr);
@@ -105,8 +130,8 @@ contract Acta {
         if(nRequiredVotes == 1) executeProposal(nProposals-1);
     }
 
-    ////PROPOSAL VOTING & REVOKING
-
+    // @dev Member gets to vote a proposal and if gets enough amount of votes, gets executed
+    // @param uint proposalIndex Proposals are ordered as a list. Specifier
     function voteProposal(uint proposalIndex) public {
 		require(proposals[proposalIndex].proposalId > 0);
 		require(isMember[msg.sender] == true);
@@ -117,20 +142,19 @@ contract Acta {
 		if(proposals[proposalIndex].nVotes >= nRequiredVotes) executeProposal(proposalIndex);
     }
 
+    // @dev The opposite of above's function. Proposals that have been executed can't be reverted
 	function undoVoteProposal(uint proposalIndex) public {
-        require(proposals[proposalIndex].proposalId > 0);
-		require(isMember[msg.sender] == true);
+        require(proposals[proposalIndex].proposalId > 0 && isMember[msg.sender] == true);
 		require(votes[proposalIndex][msg.sender] == true);
 	    votes[proposalIndex][msg.sender] = false;
 		proposals[proposalIndex].nVotes--;
     }
 
-    ///PROPOSAL EXECUTION
-
+    // @dev Can only be called internally. Executes proposals which have enough required votes
+    // @param uint proposalIndex The index of the proposal you want to execute
     function executeProposal(uint proposalIndex) private {
     	require(proposals[proposalIndex].proposalId > 0);
         require(proposals[proposalIndex].nVotes >= nRequiredVotes);
-        require(proposals[proposalIndex].proposalId > 0);
         Proposal memory proposal = proposals[proposalIndex];
 
         if(proposal.proposalId == 1) {
@@ -157,34 +181,44 @@ contract Acta {
         }
         emit ProposalExecuted("ProposalExecuted", proposal.proposalId, proposal.numData, proposal.bytesData1, proposal.bytesData2, proposal.addrData);
     }
-
-    //EXTERNAL QUERY FUNCTIONS
-
+    
+    // @dev Gets queried by members and non-members
+    // @param address addr Function tells you if this address has membership
+    // @returns a boolean value -> true if addr is a member and false if it isn't registered
     function _isMember(address addr) public view returns (bool) {
         if(isMember[addr]) return true;
-        else return false;
+        return false;
     }
 
+    // @dev Gets queried by members and non-members
+    // @param address addr The address of the member you wanna query info
+    // @returns Data about the member (name, gateway)
     function getMember(address addr) public view returns (bytes32, bytes32) {
+        require(isMember[addr] == true);
         return (members[addr].name, members[addr].gateway);
     }
 
+    // @dev Lookup the ETH address of a member by using the name
+    // @returns The address of the member with the input name
     function getAddressByName(bytes32 name) public view returns (address) {
         return nameToAddress[name];
     }
 
+    // @dev Returns all the data about a proposal
+    // @returns (id, numData, strData1, strData2, addrData, nVotes)
     function getProposal(uint proposalIndex) public view returns (uint, uint, bytes32, bytes32, address, uint) {
         return (proposals[proposalIndex].proposalId, proposals[proposalIndex].numData, proposals[proposalIndex].bytesData1,
         proposals[proposalIndex].bytesData2, proposals[proposalIndex].addrData, proposals[proposalIndex].nVotes);
     }
 
+    // @dev Members call this function to change their gateways
+    // @param bytes32 newGateway The new gateway for the member caller of the function
     function changeGateway(bytes32 newGateway) public {
         require(isMember[msg.sender] == true);
         members[msg.sender].gateway = newGateway;
         emit GatewayChanged("GatewayChanged", members[msg.sender].name, newGateway);
     }
-
-    function() external payable {
-
-    }
+    
+    // @dev To be able to receive external funds. Fallback function.
+    function() external payable {}
 }
--- profile.sol
+++ profile.sol
@@ -29,7 +29,7 @@ contract Profile {
         admin = msg.sender;
     }
     
-    @dev Function gets called by health provider, which must be certified in the acta, which establishes relationship with patient owner of this contract @admin
+    // @dev Function gets called by health provider, which must be certified in the acta, which establishes relationship with patient owner of this contract @admin
     function newRelationship() public {
         //query Acta to see if it's a certified member
         require(acta._isMember(msg.sender) == true);
@@ -47,8 +47,8 @@ contract Profile {
         names.push(name);
     }
 
-    @dev admin of the contract ends a relationship. Data of the relationship will be kept to avoid computations but it will be marked as non-existent
-    @param bytes32 name Name of the relationship (the name of the provider)
+    // @dev admin of the contract ends a relationship. Data of the relationship will be kept to avoid computations but it will be marked as non-existent
+    // @param bytes32 name Name of the relationship (the name of the provider)
     function deleteRelationship(bytes32 name) public {
         require(msg.sender == admin);
         if(relationships[name].exists == false) { revert(); }
@@ -64,12 +64,12 @@ contract Profile {
         nRelationships--;
         relationships[name].exists = false;
     }
-
-    @dev admin grants a certain address @addr access to a section of a relationship's data for an indefinite or specific amount of time
-    @param bytes32 name The name of the relationship the admin changes access permissions (Can't be "all")
-    @param address addr The address / Ethereum account which will be granted access to some information
-    @param bytes32 section The section the address will be able to see (Can be "all" for full access)
-    @param uint nHours The number of hours the address will be granted access. If nHours == 0 it will have access until it manually gets revoked by admin
+    
+    // @dev admin grants a certain address @addr access to a section of a relationship's data for an indefinite or specific amount of time
+    // @param bytes32 name The name of the relationship the admin changes access permissions (Can't be "all")
+    // @param address addr The address / Ethereum account which will be granted access to some information
+    // @param bytes32 section The section the address will be able to see (Can be "all" for full access)
+    // @param uint nHours The number of hours the address will be granted access. If nHours == 0 it will have access until it manually gets revoked by admin
 	function grantAccess(bytes32 name, address addr, bytes32 section, uint nHours) public {
 		require(msg.sender == admin);
         if(nHours > 0) relationships[name].permissions[addr].fullAccessRevokeAt = block.timestamp + (1 hours * nHours);
@@ -77,10 +77,10 @@ contract Profile {
         else relationships[name].permissions[addr].cats.push(section)
 	}
 
-    @dev admin revokes a certain address @addr access to the section (Can be "all") to the data included in a relationship, which goes by name @name
-    @param bytes32 name The name of the relationship
-    @param address addr The address which will be taken out access
-    @param bytes32 section The name of the section
+    // @dev admin revokes a certain address @addr access to the section (Can be "all") to the data included in a relationship, which goes by name @name
+    // @param bytes32 name The name of the relationship
+    // @param address addr The address which will be taken out access
+    // @param bytes32 section The name of the section
 	function revokeAccess(bytes32 name, address addr, bytes32 section) public {
 		require(msg.sender == admin);
         if(section == "all") relationships[name].permissions[addr].fullAccess = false;
@@ -95,12 +95,12 @@ contract Profile {
             relationships[name].permissions[addr].cats = cats;
         }
 	}
-
-    @dev Function to query the contract if a certain address @addr has access to @section included in relationship called @name
-    @param bytes32 name Name of the relationship which includes the data
-    @param address addr The address we want to know the data about
-    @param bytes32 section The section of the data inside the relationship
-    @returns Returns true if @addr has access to that section inside that relationship, otherwise returns false
+    
+    // @dev Function to query the contract if a certain address @addr has access to @section included in relationship called @name
+    // @param bytes32 name Name of the relationship which includes the data
+    // @param address addr The address we want to know the data about
+    // @param bytes32 section The section of the data inside the relationship
+    // @returns Returns true if @addr has access to that section inside that relationship, otherwise returns false
     function hasAccess(bytes32 name, address addr, bytes32 section) public view returns (bool) {
         require(relationships[name].exists == true);
         if(addr == admin || relationships[name].permissions[addr].fullAccess == true) return true;
@@ -109,8 +109,8 @@ contract Profile {
         return false;
     }
 
-    @dev Function to query the contract the list of all the API gateways which store data about the admin of the contract
-    @returns Returns a list of strings, whereas each string is an IP and a Port to access the API
+    // @dev Function to query the contract the list of all the API gateways which store data about the admin of the contract
+    // @returns Returns a list of strings, whereas each string is an IP and a Port to access the API
     function returnGateways() public view returns (bytes32[] memory) {
         bytes32[] memory out;
         for(uint i = 0; i < nRelationships; i++) {
@@ -119,8 +119,8 @@ contract Profile {
         return out;
     }
 
-    @dev Function to query the contract the list of all relationships the admin of the contract has
-    @returns Returns a list of strings, whereas each string is the name of a provider, which has a relationship with the admin of the contract
+    // @dev Function to query the contract the list of all relationships the admin of the contract has
+    // @returns Returns a list of strings, whereas each string is the name of a provider, which has a relationship with the admin of the contract
     function returnNames() public view returns (bytes32[] memory) {
         bytes32[] memory out;
         for(uint i = 0; i < nRelationships; i++) {
@@ -129,8 +129,8 @@ contract Profile {
         return out;
     }
 
-    @param bytes32 name The name of the relationship you want to query
-    @returns Returns some data about the relationship including: the address of the health provider, gateway to access its API
+    // @param bytes32 name The name of the relationship you want to query
+    // @returns Returns some data about the relationship including: the address of the health provider, gateway to access its API
     function getRelationship(bytes32 name) public view returns (address, bytes32) {
         require(relationships[name].exists == true);
         return (relationships[name].addr, relationsips[name].gateway);
